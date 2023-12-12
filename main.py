82858dfc7b31c5ce03752d7e5b8ea6a7c13bf482
zp y <yangzp159@gmail.com>
2023-12-12 20:33:18 +0800

    2023/12/10 12:00

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import numpy as np
+from perceptron import Perceptron
 if __name__ == '__main__':
-    print('PyCharm')
-    batch_X=np.array([[1,2,3],[4,5,6],[7,8,9]])
-    batch_y=np.array([[1],[-1],[1]])
-    Gram = batch_X.dot(batch_X.T)
-    print(Gram)
+    batch_X=np.array([[-1,4,3],[4,0,9],[7,-3,-40],[-8,0,-9],[5,5,2],[5,-242,342],[-42,-4,-2],[12,3,-43],[-4,34,2],[-43,2,4],[-43,-2,-4]])
+    batch_Y=np.array([-1,1,1,-1,1,1,-1,1,-1,-1,-1])
+    X=np.array([[-7,9,-5]])
+    perceptron=Perceptron(3,0.1,batch_X,batch_Y)
+    perceptron.train(50)
+    print(perceptron.forward(X))
--- perceptron.py
+++ perceptron.py
@@ -2,22 +2,35 @@ import numpy as np
 
 
 class Perceptron:
-    def __init__(self,dim,learning_rate):
+    def __init__(self,dim,learning_rate,batch_X,batch_Y):
         self.dim=dim
-        self.weights=np.random.randn(dim)
+        self.batch = batch_X.shape[0]
+        self.weights=np.random.randn(self.batch)
         self.biases=np.random.randn(1)
         self.learning_rate=learning_rate
+        self.batch_X=batch_X
+        self.batch_Y=batch_Y
+        self.Gram=batch_X.dot(batch_X.T)
+
 
     def weights_init(self):
         self.weights=0
         self.biases=0
 
     def forward(self,X):
-        output= np.sign( np.dot(self.weights,X)+self.biases )
+        temp=np.zeros(self.dim)
+        for i in range(self.batch):
+            print()
+            temp+=(self.weights[i]*self.batch_Y[i])*self.batch_X[i]
+        output= np.sign( np.dot(temp,X.T)+self.biases )
+        return output
 
-    def train(self,batch_X,batch_Y,epoch):
-        Gram=batch_X.dot(batch_X.T)
-        batch=batch_X.shape(0)
+    def train(self,epoch):
         for i in range(epoch):
-            choose_data=np.random.randint(0,batch)
-            output=batch_Y[choose_data]*()
+            choose_data=np.random.randint(0,self.batch)
+            temp=0
+            for j in range(self.batch):
+                temp+=self.weights[j]*self.batch_Y[j]*self.Gram[j][choose_data]
+            if self.batch_Y[choose_data]*(temp+self.biases)<=0:
+                self.weights[choose_data]+= self.learning_rate
+                self.biases+= self.learning_rate*self.batch_Y[choose_data]
